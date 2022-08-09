3496226e94ab4d5ec9cd4d5ec765efff5a52f5b6
Senura Jayasinghe <senurajayasinghe.se@gmail.com>
2022-08-09 23:58:39 +0530

    initial api for  notes

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,26 +1,54 @@
 const asyncHandler = require('express-async-handler')
+
+const Note = require('../models/noteModel')
+
+
 // @route GET /api/notes
 const getNotes = asyncHandler( async (req, res) => {
-    res.json({message:'Get notes'})
+    const notes = await Note.find()
+    res.json(notes)
 })
 
 // @route POST /api/notes
 const setNotes = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
+    if(!req.body.title) {
         res.status(400)
-        throw new Error('please add a text field')
+        throw new Error('please add a title')
+
+    } else if(!req.body.description){
+        res.status(400)
+        throw new Error('please add a description')
     }
-    res.json({message:'Set notes'})
+    const note = await Note.create({
+        title: req.body.title,
+        description: req.body.description
+    })
+    res.json(note)
 })
 
 // @route PUT /api/notes/:id
 const updateNotes = asyncHandler (async (req, res) => {
-    res.json({message:`Update note ${req.params.id}`})
+    const note = await Note.findById(req.params.id)
+        if(!note) {
+            res.status(400)
+            throw new Error('Note not found')
+        }
+            const updatedNote = await Note.findByIdAndUpdate(req.params.id,
+                 req.body,{new: true})
+
+    res.json(updatedNote)
 })
 
 // @route DELETE /api/notes/: id
 const deleteNotes = asyncHandler(async (req, res) => {
-    res.json({message:`Delete note ${req.params.id}`})
+    const note = await Note.findById(req.params.id)
+    if(!note) {
+        res.status(400)
+        throw new Error('Note not found')
+    
+    }  await note.remove()
+
+    res.json({id: req.params.id} )
 })
 
 module.exports = {
